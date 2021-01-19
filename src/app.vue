152a9dca5306139108898df17021de3f2bbe14cd
longlonglaobiao <2719330946@qq.com>
2021-01-19 19:32:48 +0800

    first

---
+++ src/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Hello></Hello>
+  </div>
+</template>
+<script>
+import Hello from "@/components/hello.vue";
+export default {
+  components: {
+    Hello,
+  },
+};
+</script>
---
+++ src/components/hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="con">
+    {{ msg }}
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: "hello world",
+    };
+  },
+};
+</script>
+<style lang="less">
+.con {
+  color: red;
+}
+</style>
